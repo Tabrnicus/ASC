dff1bda799c9c6f181f452102dfddf6c6a0aadee
Nectarios Chroniaris <nchroniaris@gmail.com>
2020-04-28 18:41:18 -0600

    initial project commit with Gradle

---
+++ src/main/java/com/nchroniaris/ASC/client/Main.java
@@ -0,0 +1,17 @@
+package com.nchroniaris.ASC.client;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Test");
+
+    }
+
+    public static int example() {
+
+        return 1;
+
+    }
+
+}
---
+++ src/test/java/com/nchroniaris/ASC/client/MainTest.java
@@ -0,0 +1,15 @@
+package com.nchroniaris.ASC.client;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MainTest {
+
+    @Test
+    public void exampleTest() {
+
+        Assert.assertEquals(Main.example(), 1);
+
+    }
+
+}
