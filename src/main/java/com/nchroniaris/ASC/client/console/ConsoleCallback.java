c9f9fa6b65eee25d28079a4cce993b63dabdbe7a
Nectarios Chroniaris <nchroniaris@gmail.com>
2020-12-31 17:06:48 -0700

    Add scheduleEvent() to callback class

--- src/main/java/com/nchroniaris/ASC/client/console/ConsoleCallback.java
+++ src/main/java/com/nchroniaris/ASC/client/console/ConsoleCallback.java
@@ -1,9 +1,21 @@
 package com.nchroniaris.ASC.client.console;
 
+import com.nchroniaris.ASC.client.model.Event;
+
+import java.util.concurrent.Future;
+
 /**
  * This interface serves as a callback for the console. Specifically, each method would be a common action that the console needs to perform on the ASCClient.
  */
 public interface ConsoleCallback {
+
+    /**
+     * Schedules an event (defined by the parameter) as soon as possible.
+     *
+     * @param event The event to schedule
+     * @return A {@code Future} that represents the future status of the event
+     */
+    Future<?> scheduleEvent(Event event);
 
     /**
      * Shuts down the ASCClient gracefully, respecting events that are currently executing
