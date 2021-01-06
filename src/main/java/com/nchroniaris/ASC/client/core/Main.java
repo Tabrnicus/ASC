2798372618f2af7a07715a381bddeb606123337d
Nectarios Chroniaris <nchroniaris@gmail.com>
2021-01-05 17:48:47 -0700

    revamp argument processing and add 2 new arguments
    - This should make adding new arguments easier but it may need to be changed later

--- src/main/java/com/nchroniaris/ASC/client/core/Main.java
+++ src/main/java/com/nchroniaris/ASC/client/core/Main.java
@@ -1,31 +1,50 @@
 package com.nchroniaris.ASC.client.core;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
-    private static final String USAGE_STRING = "java -jar ASC-client.jar [-h|-s]";
+    private static final String USAGE_STRING = "java -jar ASC-client.jar [-h|-s|-c|-d]";
 
     public static void main(String[] args) {
 
         ASCClient.ClientOptions options = new ASCClient.ClientOptions();
 
+        // Split single combination arguments into multiple arguments so that the rest of this function can process them correctly
+        List<String> convertedArgs = Main.convertArguments(args);
+
         // Check all arguments. If we don't have any, this loop will not run and it will just run the client normally with networking. If we have exactly one arg it will be checked and handled appropriately. If we have more than one argument it will either print the help text or it will give an error.
-        for (String arg : args) {
+        for (String arg : convertedArgs) {
 
-            // Print help and usage information
-            if (arg.equals("-h")) {
+            switch (arg) {
 
-                Main.printHelp();
-                return;
+                // Print help and usage information and quit
+                case "-h":
+                case "--help":
+                    Main.printHelp();
+                    return;
 
-            } else if (arg.equals("-s")) {
+                case "-s":
+                case "--serverless":
+                    options.serverless = true;
+                    break;
 
-                options.serverless = true;
+                case "-c":
+                case "--console-only":
+                    options.consoleOnly = true;
+                    break;
 
-            } else {
+                case "-d":
+                case "--allow-dumb-terminal":
+                    options.allowDumbTerminal = true;
+                    break;
 
-                System.out.println(String.format("Unrecognized option '%s'", arg));
-                Main.printUsage();
-                return;
+                // Print error message, usage, and exit
+                default:
+                    System.out.printf("Unrecognized option '%s'%n", arg);
+                    Main.printUsage();
+                    return;
 
             }
 
@@ -49,21 +68,66 @@ public class Main {
     }
 
     /**
+     * Creates a new list of String arguments, with single combination arguments, like "-xyz" into full arguments, like "-x", "-y", and "-z" while preserving order.
+     * @param programArgs A primitive String array that represents the current argument list
+     * @return A {@code List<String>} that expands combination arguments into multiple entries, but is otherwise the same.
+     */
+    private static List<String> convertArguments(String[] programArgs) {
+
+        List<String> convertedArgs = new ArrayList<>();
+
+        // Iterate through all the arguments given by the OS
+        for (String arg : programArgs) {
+
+            // Trim the argument to allow things like " -h" to get through
+            arg = arg.trim();
+
+            // If the argument is a single-dash argument (the character class is for safety), with 1 or more arguments (like "-scd" for example)
+            if (arg.matches("^-[A-Za-z0-9]+$")) {
+
+                // For every letter (index 1 and onward, basically excluding the dash), add that as a new entry in the new list. This essentially splits arguments like "-scd" multiple ones: "-s", "-c", and "-d".
+                for (int i = 1; i < arg.length(); i++)
+                    convertedArgs.add("-" + arg.charAt(i));
+
+            } else {
+
+                // If it's not a single argument in that format just add the argument verbatim. This case will execute for double-dashed arguments, like "--help"
+                convertedArgs.add(arg);
+
+            }
+
+        }
+
+        return convertedArgs;
+
+    }
+
+    /**
      * Prints out the full usage and help information. This includes the usage text, link to the GitHub repo, options summary, and additional information.
      */
     private static void printHelp() {
 
-        System.out.println(String.format("Usage: %s\n", Main.USAGE_STRING));
+        System.out.printf("Usage: %s%n%n", Main.USAGE_STRING);
         System.out.println("ASC-client is a Java application used for controlling game severs.");
         System.out.println("See https://github.com/Tardnicus/ASC for guides and more detailed information.\n");
 
         System.out.println("Option Summary:");
-        System.out.println("  -h\tShows this help menu");
-        System.out.println("  -s\tRuns in serverless mode (see below)");
+
+        System.out.println("\t-h, --help");
+        System.out.println("\t\tShows this help menu");
         System.out.println();
 
-        System.out.println("Serverless Mode:");
-        System.out.println("  When this mode is active, the client will not attempt to register any of its game servers with the master server. In other words all network functionality will be disabled completely. This is helpful if you have no need to connect it to a master server or otherwise do not want remote commands to be executed on the machine that's running this client.");
+        System.out.println("\t-s, --serverless");
+        System.out.printf("\t\tRuns in serverless mode. This will disable all networking functionality and the client will never attempt to register any of its game servers with the ASC server (if configured).%n%n\t\tThis is helpful if you have no need to connect it to an ASC server or otherwise do not want remote commands to be executed on the machine that's running this client.%n");
+        System.out.println();
+
+        System.out.println("\t-c, --console-only");
+        System.out.printf("\t\tRuns the client in console only mode. When run with this flag the client will not attempt to automatically schedule events upon startup.%n%n\t\tThis is helpful if you just want to execute manual commands in the console without accidentally starting anything automatically.%n");
+        System.out.println();
+
+        System.out.println("\t-d, --allow-dumb-terminal");
+        System.out.printf("\t\tAllow a dumb terminal to be attached to the front-facing UI. Currently, if on startup the client cannot find a properly configured terminal to attach to, it will exit. This option allows a dumb terminal to be attached, which means that some terminals may now work but with limited or undefined functionality in terms of the operation of the console (the core of the program should be undisturbed).%n%n\t\tUsually this option should not be considered, as most standard terminals work out of the box. However if you are having trouble try using this option and see if it works.%n");
+        System.out.println();
 
     }
 
@@ -72,8 +136,9 @@ public class Main {
      */
     private static void printUsage() {
 
-        System.out.println(String.format("Usage: %s", Main.USAGE_STRING));
+        System.out.printf("Usage: %s%n", Main.USAGE_STRING);
         System.out.println("Try 'java -jar ASC-client.jar -h' for more information.");
+        System.out.println();
 
     }
 
